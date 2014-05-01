2e7a2a4d73c255e47f68a1116de9af6d11e28d5a
melanie <m.anderson-12@student.lboro.ac.uk>
2014-05-01 13:08:13 +0100

    Regenerated model according to db

--- TimetablingSystem1/TimetablingSystem1/Models/TimetablingDB.Designer.cs
+++ TimetablingSystem1/TimetablingSystem1/Models/TimetablingDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\melanie\documents\visual studio 2013\Projects\TimetablingSystem1\TimetablingSystem1\Models\TimetablingDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\melanie\Documents\GitHub\TP3NEW\TimetablingSystem1\TimetablingSystem1\Models\TimetablingDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
